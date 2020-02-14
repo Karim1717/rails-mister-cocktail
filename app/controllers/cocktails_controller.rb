ca9e0e76e12fe365aa9ce5026d726acba1e6bcb8
Karim Azib <karim.azib17@gmail.com>
2020-02-14 16:38:10 +0100

    done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -10,6 +10,7 @@ class CocktailsController < ApplicationController
 
   def show
     @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
   end
 
   def create
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -5,51 +5,52 @@ class DosesController < ApplicationController
     @dose = Dose.new
   end
 
-  def index
-    @doses = Dose.all
-  end
+  # def index
+  #   @doses = Dose.all
+  # end
 
-  def show
-    @dose = Dose.find(params[:id])
-  end
-def create
-    @dose = Dose.new(dose_params)
+  # def show
+  #   @dose = Dose.find(params[:id])
+  # end
 
-    # Will raise ActiveModel::ForbiddenAttributesError
-    if @dose.save
-      redirect_to dose_path(@dose)
-    else
-      render :new
-    end
-  end
-
-  def edit
-    @dose = Dose.find(params[:id])
-  end
-
-  def update
-    @dose = Dose.find(params[:id])
-
-    # Will raise ActiveModel::ForbiddenAttributesError
-      if @dose.update(dose_params)
-        redirect_to doses_path
+  def create
+      @cocktail = Cocktail.find(params[:cocktail_id])
+      @dose = Dose.new(dose_params)
+      @dose.cocktail = @cocktail
+      if @dose.save
+        redirect_to cocktail_path(@cocktail)
       else
-        render 'edit'
+        render "cocktails/show"
       end
-  end
+    end
+
+  # def edit
+  #   @dose = Dose.find(params[:id])
+  # end
+
+  # def update
+  #   @dose = Dose.find(params[:id])
+
+  #   # Will raise ActiveModel::ForbiddenAttributesError
+  #     if @dose.update(dose_params)
+  #       redirect_to doses_path
+  #     else
+  #       render 'edit'
+  #     end
+  # end
 
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
 
     # no need for app/views/doses/destroy.html.erb
-    redirect_to doses_path
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:name, :description, :ingredient)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 
 end
